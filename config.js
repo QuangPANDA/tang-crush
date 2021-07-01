b6e8c9dc5c755280bd8a2140eca0c18896481239
QuangPANDA <86741141+QuangPANDA@users.noreply.github.com>
2021-07-01 11:44:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Trinh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao bà cứ đi nhầm đường
+    Lạc vào tim tui lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Nếu bây giờ tớ nói thích cậu 😙',
+    desc: 'Thì cậu sẽ trả lời như thế nào ',
+    btnYes: 'Ok thích thì quen <33',
+    btnNo: 'Không mình làm bạn :3',
+    question:'Trên thế giới hơn 7 tỉ người mà tớ chỉ thích mỗi Trinh <3',
+    btnReply: 'Gửi cho Quang <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Quang biết mà 🥰. Love Trinh <3 3000 😘😘',
+    messDesc: 'Tối nay 8H30 nhảy dù nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
